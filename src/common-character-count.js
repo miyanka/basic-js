24f989fbca3fa3803115102e165e743f1ff7017c
Yana <bojcenko.jana@gmail.com>
2023-12-17 19:15:05 +0100

    feat: implement common-character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  const countChar = (str, char) => [...str].filter((i) => i === char).length;
+
+  [...new Set(s1)].forEach((item) => {
+    count += Math.min(countChar(s1, item), countChar(s2, item));
+  });
+
+  return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
